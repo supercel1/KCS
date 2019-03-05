675f0ffd8325910fc23ec1c277d1b48d7eeb5ad0
supercell <undermountainafu@icloud.com>
2019-03-05 23:55:08 +0900

    add template

--- kcs/settings.py
+++ kcs/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+## Authentication
+LOGIN_REDIRECT_URL = 'home'
