dd3dcb267cb3bfb43c8eedad8c50afbb05513b45
supercell <undermountainafu@icloud.com>
2019-03-07 21:53:10 +0900

    fix views.py

--- users/views.py
+++ users/views.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-
-# Create your views here.
-=======
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 class SignupView(CreateView):
-    form_class = UseCreationForm
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
->>>>>>> origin/master
