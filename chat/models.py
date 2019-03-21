4be61ed6ed433eb68315a56e8af04171c1ee917c
supercell <undermountainafu@icloud.com>
2019-03-21 22:37:53 +0900

    fixing module

--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from datetime import timezone, datetime
+from datetime import timezone
 
 class Room(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     label = models.CharField(max_length=100, blank=True, null=True)
     is_private = models.BooleanField(default=True)
-    created_at = models.DateTimeField(default=datetime.now())
+    created_at = DatetimeField(default=timezone.now())
 
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
     handle = models.TextField()
     message = models.CharField(max_length=150, blank=True, null=True)
-    created_at = models.DateTimeField(default=datetime.now(), db_index=True)
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
