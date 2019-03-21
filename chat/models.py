72671e71bc7e7d4c425366c1089573afa4a2e147
supercell <undermountainafu@icloud.com>
2019-03-21 23:05:51 +0900

    fix chat

--- chat/models.py
+++ chat/models.py
@@ -5,7 +5,7 @@ class Room(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     label = models.CharField(max_length=100, blank=True, null=True)
     is_private = models.BooleanField(default=True)
-    created_at = DatetimeField(default=timezone.now())
+    created_at = models.DateTimeField(default=timezone.now())
 
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
